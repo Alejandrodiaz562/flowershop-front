c9675cf425bce5848b3c18c61310f988f40b2683
Alejandrodiaz562 <111014821+Alejandrodiaz562@users.noreply.github.com>
2026-01-31 23:33:50 -0500

    fixing errors

--- src/components/catalog/CategoryPreview.jsx
+++ src/components/catalog/CategoryPreview.jsx
@@ -15,7 +15,7 @@ const CategoryPreview = ({category, myData}) => {
     return ( 
         <div className={`
             min-h-70 h-auto
-            border-3 border-black
+            border-3 border-purple-600
             bg-amber-300`}
         >
             <div
@@ -38,12 +38,30 @@ const CategoryPreview = ({category, myData}) => {
             >
                 {   
                 
-                    filteredData.map(el => (
+                    filteredData.slice(0, 4).map(el => (
                         <RenderProduct product = {el}></RenderProduct>
                     ))
+
+                    
+                }
+                
+            </div>
+            
+            <div>
+                {
+                    filteredData.length > 4 &&(
+                        <div
+                            className={`
+                                border-3 border-blue-500
+                                w-full
+                                text-center
+                                `}
+                        >
+                            <button>VER TODOS</button>
+                        </div>
+                    )
                 }
             </div>
-
         </div>    
     );
 }
--- src/components/layout/MainContent.jsx
+++ src/components/layout/MainContent.jsx
@@ -48,6 +48,8 @@ const MainProducts = () => {
                         /> 
                         
                     ))
+
+                    
                 }
             />
                 <h2>{console.log(myData)}</h2>
